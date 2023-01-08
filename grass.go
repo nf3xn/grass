9da6364699a2f1bc57b905fe41a3884f01aa5634
hmcg <hmcg@dorknet.work>
2023-01-08 15:19:42 +0000

    ditch flags for one param

--- grass.go
+++ grass.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-    "encoding/json"
-    "flag"
-    "fmt"
-    "io/ioutil"
-    "os"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 
-    "github.com/mmcdole/gofeed"
+	"github.com/mmcdole/gofeed"
 )
 
-// JSON list of feeds to read from with hashtags
 type feedConfig struct {
 	Name     string   `json:"name"`
 	URL      string   `json:"url"`
@@ -18,39 +16,42 @@ type feedConfig struct {
 }
 
 func main() {
-    configFile := flag.String("config", "config.json", "Path to the config file")
-    flag.Parse()
+	var configPath string
+	if len(os.Args) > 1 {
+		configPath = os.Args[1]
+	} else {
+		configPath = "config.json"
+	}
 
-    configData, err := ioutil.ReadFile(*configFile)
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	configData, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    var feeds []feedConfig
-    err = json.Unmarshal(configData, &feeds)
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	var feeds []feedConfig
+	err = json.Unmarshal(configData, &feeds)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    fp := gofeed.NewParser()
-    for _, feed := range feeds {
+	fp := gofeed.NewParser()
+	for _, feed := range feeds {
 		fmt.Println("Name:", feed.Name)
 		fmt.Println("Feed:", feed.URL)
 		fmt.Println("Hashtags:", feed.Hashtags)
-        
-        parsedFeed, err := fp.ParseURL(feed.URL)
-        if err != nil {
-            fmt.Println(err)
-            continue
-        }
 
-        fmt.Println(parsedFeed.Title)
-        for _, item := range parsedFeed.Items {
-            fmt.Println(item.Title)
-        }
-        fmt.Println()
-    }
+		parsedFeed, err := fp.ParseURL(feed.URL)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		fmt.Println(parsedFeed.Title)
+		for _, item := range parsedFeed.Items {
+			fmt.Println(item.Title)
+		}
+		fmt.Println()
+	}
 }
-
